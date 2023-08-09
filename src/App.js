45aeed48b02113f95744b88813c0519b332fb153
alanrom <romeroa@MacBook-Air-de-Jesus.local>
2023-08-09 01:43:31 -0600

    added components and style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
 import logo from './logo.svg';
 import './App.css';
+import {buz, businessLists} from './components/Business.js'
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar'
+import Stack from 'react-bootstrap/Stack';
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    return (
+      <div>
+        <Container className="topContainer">
+        <Row className="SearchOptions">
+          <Col></Col>
+          <Col sm={3}>  Best <br></br> Match </Col>
+          <Col sm={3}> Highest <br></br> Rated </Col>
+          <Col sm={3}> Most <br></br>Reviewed </Col>
+          <Col></Col>
+        </Row> 
+        <Row> 
+          <Col md={2}></Col>
+          <Col md={8}><hr></hr> </Col>
+          <Col md={2}></Col>
+        </Row>
+        <Row className="searchBarContainer">
+          <Col></Col> 
+          <Col md={5}> <input id='searchBar' type="text" placeholder="Search Businesses"/> </Col>
+          <Col md={5}> <input id='searchBar' placeholder="Where"/> </Col>
+          <Col></Col> 
+        </Row>
+        <Row>
+        <Col></Col>
+        <Col xl={2}><Button id="SearchButton" variant="success">Let's go</Button> </Col>
+        <Col></Col>
+        </Row>
+        
+        </Container>
+      <div>
+      <Container id="imagesContainer">
+        <Row>
+      {businessLists.map(d =>
+
+          <Col md={4}>
+          <span>{d.name}</span><br></br>
+          <span>{d.category}</span><br></br>
+          <span>{d.address}</span><br></br>
+          <img src={d.imageSrc} width = '200'></img><br></br>
+          
+          
+        <br></br>
+        </Col>
+      )}
+  
+      </Row>
+      </Container>
+      </div>
+
+      
+      <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+      integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+      crossorigin="anonymous"
+    />
+      </div>
+    );
+    
+  }
+
 
 export default App;
---
+++ src/components/Business.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+const buz = {
+    imageSrc : 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+    name : 'Romero Repostería',
+    address : 'Ave Vasconcelos',
+    city : 'San Pedro Garza García',
+    state : 'Nuevo León',
+    zipcode : '62650',
+    category : 'Desserts',
+    rating : 4.5,
+    reviewCount : 308
+}
+
+const businessLists = [
+{
+    imageSrc : 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+    name : 'Romero Pizzas',
+    address : 'Ave Gonzalitos',
+    city : 'Monterrey',
+    state : 'Nuevo León',
+    zipcode : '64560',
+    category : 'Pizza',
+    rating : 4.7,
+    reviewCount : 209
+},
+{
+    imageSrc : 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+    name : 'Romero Burgers',
+    address : 'Ave Garza Sada',
+    city : 'Monterrey',
+    state : 'Nuevo León',
+    zipcode : '62650',
+    category : 'Burgers',
+    rating : 4.4,
+    reviewCount : 450
+},
+{    imageSrc : 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+name : 'Romero Elotes',
+address : 'Ave Central',
+city : 'San Pedro Garza García',
+state : 'Nuevo León',
+zipcode : '62654',
+category : 'Desserts',
+rating : 4.1,
+reviewCount : 500
+},
+{    imageSrc : 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+name : 'Romero Raspados',
+address : 'Calzada San Pedro',
+city : 'San Pedro Garza García',
+state : 'Nuevo León',
+zipcode : '62650',
+category : 'Desserts',
+rating : 4.8,
+reviewCount : 318
+},
+{    imageSrc : 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+name : 'Romero Donuts',
+address : 'Ave Gomez Morín',
+city : 'San Pedro Garza García',
+state : 'Nuevo León',
+zipcode : '62366',
+category : 'Desserts',
+rating : 4.8,
+reviewCount : 239
+}]
+
+
+export {buz, businessLists}
